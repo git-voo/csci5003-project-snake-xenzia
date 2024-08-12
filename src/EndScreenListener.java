94c7a5997121ae34eb09d5e5992b5aeeec03ea13
UcheWendy <wendy.onwuagana@slu.edu>
2024-08-11 20:55:47 -0500

    implemented 3 interfaces

---
+++ src/EndScreenListener.java
@@ -0,0 +1,5 @@
+public interface EndScreenListener {
+    void onRetry();
+ 
+    void onExit();
+ }
---
+++ src/GameOverListener.java
@@ -0,0 +1,5 @@
+public interface GameOverListener {
+
+    public void onGameOver(int finalScore);
+ }
+ 
